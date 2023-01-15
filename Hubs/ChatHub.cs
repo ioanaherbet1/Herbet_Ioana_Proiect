fb9d5519d851c9a9a61245b4612ad8d91082a1af
ioana <ioana@IOANA>
2023-01-15 02:07:33 +0200

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace Herbet_Ioana_Proiect.Hubs
+
+{
+    public class ChatHub :Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
+        }
+    }
+
+}
